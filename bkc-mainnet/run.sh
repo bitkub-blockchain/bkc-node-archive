f5b036413888950bb3c573470997134463f8f3f2
owen.kamin <owen.kamin@bitkub.com>
2021-04-30 08:41:07 +0000

    [ADD] bitkub text inside script file.

--- bkc-mainnet/run.sh
+++ bkc-mainnet/run.sh
@@ -7,19 +7,27 @@
 clear
 version="v.0.0.2"
 
-# Function
-
 #
 # Color section.
 #
 
-RED='\033[0;31m' # Red
-GRE='\033[0;32m' # Green
+RED='\033[0;91m' # Red
+GRE='\033[0;92m' # Green
 YEL='\033[1;33m' # Yellow
 BLU='\033[0;34m' # Blue
 NC='\033[0m' # No Color
+NOR='\033[0;39m'
+DGRE='\033[0;32m'
 
 # Function
+
+function dotsleep () {
+  for ((i=1;i<="$1";i++))
+  do
+    echo -e -n "${GRE}.  ${NC}"
+    sleep 1s
+  done
+}
 
 function checkit {
   check=$(ls -1 | find node0)
@@ -32,12 +40,42 @@ function checkit {
   fi
 }
 
+function ethis {
+ echo -e "${GRE}$1${NOR}"
+}
+function ethis2 {
+ echo -e "${DGRE}$1${NOR}"
+}
+
+#
+# Bitkub Logo
+#
+
+
+ethis ""
+ethis "  ____________       _______     ____________    ___         ____   ___       ___    ____________     "
+ethis "  BBBBBBBBBBBBb.    dBBBBBBD    dBBBBBBBBBBBBb   KKKb       dBBP    UUU       UUU    BBBBBBBBBBBBb.   "
+ethis "  BB           Vb      BN             BN         KKK|      dBB/      BN       BN     BB           Vb  "
+ethis "  BB___________AP      BN             BN         KKK|     dBB/       BN       BN     BB           AP  "
+ethis2 "  VVBBBBBBBBBVVK       BN             BN         VVVVVVVVVVVK        BN       BN     VVBBBBBBBBBBVK   "
+ethis2 "  BB           Vb      BN             BN         KKK|     VBB\\       bY       Yd     BB           Vb  "
+ethis2 "  BB___________AD    __BN___          BN         KKK|      VBB\\      VA       AV     BB___________AD    "
+ethis2 "  BBBBBBBBBBBBBP'   dBBBBBBP          BB         KKK|       VBDb     'UUUUUUUUU'     BBBBBBBBBBBBBP'    "
+ethis ""
+echo -n " "
+sleep 1
+echo -e -n "${GRE}[ Press any key to continue... ]${NOR}"
+read -n 1 -s
+echo ""
+sleep 1
+clear
+
+
 # Main
+checkit
 cp  bitkubchain.json genesis.json
 echo -e "RUN THIS $version\n"
 echo -e ">> running node-check" # Verifying existing node
-checkit
-
 echo -e ">> running geth-install.sh" # Verifying geth installation process
 cp scripts/geth-install.sh ./geth-tmp.sh
 ./geth-tmp.sh
