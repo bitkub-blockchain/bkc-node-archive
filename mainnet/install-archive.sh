e0bc8d4ffbfeb2919ae4899e2cf1f8ffd255bab6
Isman Usoh <isman.isman@bitkub.com>
2021-11-18 04:25:10 +0700

    fix: install script

--- mainnet/install-archive.sh
+++ mainnet/install-archive.sh
@@ -7,9 +7,13 @@ apt-get update -y
 apt-get install ethereum git -y
 
 chown -R $USER /bkc-node
-git clone https://github.com/bitkub-blockchain/bkc-node.git .
+mkdir -p /bkc-node/mainnet
 
-cp ./mainnet/services/archive.service /etc/systemd/system/geth.service
+curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/config.toml --output /bkc-node/mainnet/config.toml
+curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/genesis.json --output /bkc-node/mainnet/genesis.json
+curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/services/archive.service --output /etc/systemd/system/geth.service
+
+
 sed -i "s/<NAME>/$1/g" /etc/systemd/system/geth.service;
 
 geth --datadir /bkc-node/mainnet/data init /bkc-node/mainnet/genesis.json
