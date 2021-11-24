f63f2195855a9da8ff060e038d977beacf70e4c4
Isman Usoh <isman.isman@bitkub.com>
2021-11-24 22:12:50 +0700

    clean script

--- mainnet/install-archive.sh
+++ mainnet/install-archive.sh
@@ -11,11 +11,10 @@ curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/
 curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/genesis.json --output /bkc-node/mainnet/genesis.json
 curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/services/archive.service --output /etc/systemd/system/geth.service
 
-sed -i "s/<NAME>/$1/g" /etc/systemd/system/geth.service;
-
 geth --datadir /bkc-node/mainnet/data init /bkc-node/mainnet/genesis.json
 
 systemctl daemon-reload
+systemctl enable --now geth
 systemctl restart geth
 
 journalctl -u geth -f
