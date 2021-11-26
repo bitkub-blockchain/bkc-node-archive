cf19db0d8d5d28de95cc84f4bfcfbe20236d6e2e
Isman Usoh <isman.isman@bitkub.com>
2021-11-26 15:36:00 +0700

    fix: add mount tab

--- mainnet/mount.sh
+++ mainnet/mount.sh
@@ -7,8 +7,9 @@ sudo mount /dev/nvme1n1 /bkc-node/
 sudo chown -R $USER /bkc-node
 sudo cp /etc/fstab /etc/fstab.bak
 
+
+sudo sh -c "echo '/dev/vdb       /bkc-node   ext4    defaults,nofail        0       0' >> /etc/fstab"
+sudo mount -a
+
 cd /bkc-node
 df -h .
-
-sudo echo "/dev/vdb       /bkc-node   ext4    defaults,nofail        0       0" >> /etc/fstab
-sudo mount -a
