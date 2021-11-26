c714b0cdb73bb977927cf18b191375952c7cea77
Isman Usoh <isman.isman@bitkub.com>
2021-11-26 15:16:31 +0700

    add: script for x10

--- mainnet/install-bootnode.sh
+++ mainnet/install-bootnode.sh
@@ -8,12 +8,9 @@ apt-get install ethereum -y
 
 chown -R $USER /bkc-node
 
-curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/config.toml --output /bkc-node/mainnet/config.toml
-curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/genesis.json --output /bkc-node/mainnet/genesis.json
-curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/services/bootnode.service --output /etc/systemd/system/geth.service
-
-cp ./mainnet/services/archive.service /etc/systemd/system/geth.service
-sed -i "s/<NAME>/$1/g" /etc/systemd/system/geth.service;
+curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/x10/mainnet/config.toml --output /bkc-node/mainnet/config.toml
+curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/x10/mainnet/genesis.json --output /bkc-node/mainnet/genesis.json
+curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/x10/mainnet/services/bootnode.service --output /etc/systemd/system/geth.service
 
 geth --datadir /bkc-node/mainnet/data init /bkc-node/mainnet/genesis.json
 
