f3cfa48def66310adc7d9373631577df1056b73b
Isman Usoh <isman.isman@bitkub.com>
2021-11-29 19:04:40 +0700

    fix: bootnode script

--- mainnet/install-bootnode.sh
+++ mainnet/install-bootnode.sh
@@ -6,7 +6,7 @@ apt-get install ethereum -y
 
 mkdir -p /bkc-node/mainnet
 
-curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/config.toml --output /bkc-node/mainnet/config.toml
+curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/config.bootnode.toml --output /bkc-node/mainnet/config.toml
 curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/genesis.json --output /bkc-node/mainnet/genesis.json
 curl https://raw.githubusercontent.com/bitkub-blockchain/bkc-node/proen/mainnet/services/bootnode.service --output /etc/systemd/system/geth.service
 
